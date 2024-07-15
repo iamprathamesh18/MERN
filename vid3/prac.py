8b2ae7c8ca9ed3e7eb393e1961a78cb283de07ab
Prathamesh Shinde <121452545+iamprathamesh18@users.noreply.github.com>
2024-07-15 21:41:33 +0530

    Add files via upload

---
+++ vid3/prac.py
@@ -0,0 +1,8 @@
+word="hello"
+vowel_count=()
+for char in word:
+    if char.lower() in "aeiou":
+        vowel_count+=1
+
+        print("number of vowels;,vowel_count")
+        #output-number of vowels :2 
